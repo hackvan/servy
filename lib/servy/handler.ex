e9ce1be4d1b2efdbfb066a0e0948077c13e9e260
Diego Camacho <hackvan@gmail.com>
2020-10-18 20:39:37 -0500

    High-Level transformations

---
+++ lib/servy/handler.ex
@@ -0,0 +1,40 @@
+defmodule Servy.Handler do
+  def handler(request) do
+    # conv = parse(request)
+    # conv = route(conv)
+    # format_response(conv)
+    request
+    |> parse
+    |> route
+    |> format_response
+  end
+
+  def parse(request) do
+    conv = %{ method: "GET", path: "/wildthings", resp_body: "" }
+  end
+
+  def route(request) do
+    conv = %{ method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers" }
+  end
+
+  def format_response(conv) do
+    """
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Length: 20
+
+    Bears, Lions, Tigers
+    """
+  end
+end
+
+request = """
+GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handler(request)
+IO.puts response
