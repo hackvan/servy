53de50ad0e1a8d1df35fa8fd429fd1e14912f8c9
Diego Camacho <hackvan@gmail.com>
2020-10-20 12:00:08 -0500

    Exercise rewriting paths for SEO

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -27,6 +27,10 @@ defmodule Servy.Handler do
     %{ conv | path: "/wildthings" }
   end
 
+  def rewrite_path(%{ path: "/bears?id=" <> id } = conv) do
+    %{ conv | path: "/bears/#{id}" }
+  end
+
   def rewrite_path(conv), do: conv
 
   def log(conv), do: IO.inspect conv
@@ -138,6 +142,17 @@ IO.puts response
 
 request = """
 GET /wildlife HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handler(request)
+IO.puts response
+
+request = """
+GET /bears?id=2 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
