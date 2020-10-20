5b4343bca516848dbb505fcd54c285760207f656
Diego Camacho <hackvan@gmail.com>
2020-10-20 11:33:55 -0500

    Pattern matching maps for refactor route functions

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -23,7 +23,7 @@ defmodule Servy.Handler do
     }
   end
 
-  def rewrite_path(%{path: "/wildlife"} = conv) do
+  def rewrite_path(%{ path: "/wildlife" } = conv) do
     %{ conv | path: "/wildthings" }
   end
 
@@ -31,32 +31,28 @@ defmodule Servy.Handler do
 
   def log(conv), do: IO.inspect conv
 
-  def route(conv) do
-    route(conv, conv.method, conv.path)
-  end
-
-  def route(conv, "GET", "/wildthings") do
+  def route(%{ method: "GET", path: "/wildthings" } = conv) do
     %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
   end
 
-  def route(conv, "GET", "/bears") do
+  def route(%{ method: "GET", path: "/bears" } = conv) do
     %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
   end
 
-  def route(conv, "GET", "/bears/" <> id) do
+  def route(%{ method: "GET", path: "/bears/" <> id } = conv) do
     %{ conv | status: 200, resp_body: "Bears #{id}"}
   end
 
-  def route(conv, "DELETE", "/bears/" <> _id) do
+  def route(%{ method: "DELETE", path: "/bears/" <> _id } = conv) do
     %{ conv | status: 403, resp_body: "Deleting a bear is forbidden!"}
   end
 
   # Catch all routes:
-  def route(conv, _method, path) do
+  def route(%{ path: path } = conv) do
     %{ conv | status: 404, resp_body: "No #{path} here!"}
   end
 
-  def track(%{status: 404, path: path} = conv) do
+  def track(%{ status: 404, path: path } = conv) do
     IO.puts "Warning: #{path} is on the loose!"
     conv
   end
