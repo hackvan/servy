7ee2f7151dd72008e522f1b4d4ff3a5e6fe301db
Diego Camacho <hackvan@gmail.com>
2020-10-19 19:48:40 -0500

    Immutable data

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -10,26 +10,27 @@ defmodule Servy.Handler do
   end
 
   def parse(request) do
-    [method, path, _] = 
+    [method, path, _] =
       request
-      |> String.split("\n") 
+      |> String.split("\n")
       |> List.first
       |> String.split(" ")
 
     %{ method: method, path: path, resp_body: "" }
   end
 
-  def route(_request) do
-    _conv = %{ method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers" }
+  def route(conv) do
+    # conv = Map.put(conv, :resp_body, "Bears, Lions, Tigers")
+    %{ conv | resp_body: "Bears, Lions, Tigers" }
   end
 
-  def format_response(_conv) do
+  def format_response(conv) do
     """
     HTTP/1.1 200 OK
     Content-Type: text/html
-    Content-Length: 20
+    Content-Length: #{String.length(conv.resp_body)}
 
-    Bears, Lions, Tigers
+    #{conv.resp_body}
     """
   end
 end
