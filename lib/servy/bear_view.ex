c10a2b5c75aa49812ab1fb56af5c0addff9e1738
Diego Camacho <hackvan@gmail.com>
2020-10-27 16:53:57 -0500

    Clean spaces between require and variables def

--- lib/servy/bear_view.ex
+++ lib/servy/bear_view.ex
@@ -1,6 +1,5 @@
 defmodule Servy.BearView do
   require EEx
-
   @templates_path Path.expand("templates", File.cwd!)
 
   EEx.function_from_file :def, :index, Path.join(@templates_path, "index.eex"), [:bears]
