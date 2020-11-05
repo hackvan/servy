4e6e2f935f018f01e3a36835c6c863734aaec681
Diego Camacho <hackvan@gmail.com>
2020-11-04 21:02:34 -0500

    Read bears from a JSON file

--- lib/servy/file_handler.ex
+++ lib/servy/file_handler.ex
@@ -1,6 +1,8 @@
 defmodule Servy.FileHandler do
   alias Servy.Conv
 
+  @db_path Path.expand("db", File.cwd!)
+
   def handle_file({ :ok, content }, %Conv{} = conv) do
     %{ conv | status: 200, resp_body: content }
   end
@@ -11,5 +13,21 @@ defmodule Servy.FileHandler do
 
   def handle_file({ :error, reason }, %Conv{} = conv) do
     %{ conv | status: 500, resp_body: "File error: #{reason}" }
+  end
+
+  def read_json_file(filename) do
+    @db_path
+    |> Path.join(filename)
+    |> read_json
+  end
+
+  defp read_json(source) do
+    case File.read(source) do
+      {:ok, contents}  ->
+        contents
+      {:error, reason} ->
+        IO.inspect "Error reading #{source}: #{reason}"
+        "[]"
+    end
   end
 end
--- lib/servy/wildthings.ex
+++ lib/servy/wildthings.ex
@@ -2,18 +2,9 @@ defmodule Servy.Wildthings do
   alias Servy.Bear
 
   def list_bears do
-    [
-      %Bear{ id: 1, name: "Teddy", type: "Brown", hibernating: true },
-      %Bear{ id: 2, name: "Smokey", type: "Black" },
-      %Bear{ id: 3, name: "Paddington", type: "Brown" },
-      %Bear{ id: 4, name: "Scarface", type: "Grizzly", hibernating: true },
-      %Bear{ id: 5, name: "Snow", type: "Polar" },
-      %Bear{ id: 6, name: "Brutus", type: "Grizzly" },
-      %Bear{ id: 7, name: "Rosie", type: "Black", hibernating: true },
-      %Bear{ id: 8, name: "Roscoe", type: "Panda" },
-      %Bear{ id: 9, name: "Iceman", type: "Polar", hibernating: true },
-      %Bear{ id: 10, name: "Kenai", type: "Grizzly"}
-    ]
+    Servy.FileHandler.read_json_file("bears.json")
+    |> Poison.decode!(as: %{"bears" => [%Bear{}]})
+    |> Map.get("bears")
   end
 
   def get_bear(id) when is_integer(id) do
@@ -23,4 +14,5 @@ defmodule Servy.Wildthings do
   def get_bear(id) when is_binary(id) do
     id |> String.to_integer |> get_bear
   end
+
 end
