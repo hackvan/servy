114eb7d9ce015d753df7988494fa80062df6ed70
Diego Camacho <hackvan@gmail.com>
2020-11-07 18:48:36 -0500

    Create a HttpServerTest to Send Concurrent Requests

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -2,7 +2,6 @@ defmodule HttpServerTest do
   use ExUnit.Case
 
   alias Servy.HttpServer
-  alias Servy.HttpClient
 
   test "accepts a request on a socket and sends back a response" do
     spawn(HttpServer, :start, [4000])
@@ -11,5 +10,31 @@ defmodule HttpServerTest do
 
     assert response.status_code == 200
     assert response.body == "Bears, Lions, Tigers"
+  end
+
+  test "accepts a multiple request on a socket and sends back responses" do
+    spawn(HttpServer, :start, [4000])
+
+    parent = self()
+    max_concurrent_requests = 5
+
+    # Spawn the client processes
+    for _ <- 1..max_concurrent_requests do
+      spawn(fn ->
+        # Send the request
+        {:ok, response} = HTTPoison.get "http://localhost:4000/wildthings"
+        # Send the response back to the parent
+        send(parent, {:ok, response})
+      end)
+    end
+
+    # Await all {:handled, response} messages from spawned processes.
+    for _ <- 1..max_concurrent_requests do
+      receive do
+        {:ok, response} ->
+          assert response.status_code == 200
+          assert response.body == "Bears, Lions, Tigers"
+      end
+    end
   end
 end
