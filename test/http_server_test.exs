9c96eea7f12f81643a78505364aa6ec7bf28e5ab
Diego Camacho <hackvan@gmail.com>
2020-11-11 22:03:46 -0500

    Change HttpServerTest to Check Multiple URLs

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -15,26 +15,21 @@ defmodule HttpServerTest do
   test "accepts a multiple request on a socket and sends back responses" do
     spawn(HttpServer, :start, [4000])
 
-    parent = self()
-    max_concurrent_requests = 5
+    urls = [
+      "http://localhost:4000/wildthings",
+      "http://localhost:4000/bears",
+      "http://localhost:4000/bears/1",
+      "http://localhost:4000/wildlife",
+      "http://localhost:4000/api/bears"
+    ]
 
-    # Spawn the client processes
-    for _ <- 1..max_concurrent_requests do
-      spawn(fn ->
-        # Send the request
-        {:ok, response} = HTTPoison.get "http://localhost:4000/wildthings"
-        # Send the response back to the parent
-        send(parent, {:ok, response})
-      end)
-    end
+    urls
+    |> Enum.map(&Task.async(fn -> HTTPoison.get(&1) end))
+    |> Enum.map(&Task.await/1)
+    |> Enum.map(&assert_successful_response/1)
+  end
 
-    # Await all {:handled, response} messages from spawned processes.
-    for _ <- 1..max_concurrent_requests do
-      receive do
-        {:ok, response} ->
-          assert response.status_code == 200
-          assert response.body == "Bears, Lions, Tigers"
-      end
-    end
+  defp assert_successful_response({:ok, response}) do
+    assert response.status_code == 200
   end
 end
