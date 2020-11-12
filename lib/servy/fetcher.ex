7c4cb8b0dd2283fa91f62d3c146c7ed950de3add
Diego Camacho <hackvan@gmail.com>
2020-11-11 21:09:04 -0500

    Add an after clause to Fetcher.get_result to handle slow responses

--- lib/servy/fetcher.ex
+++ lib/servy/fetcher.ex
@@ -7,7 +7,11 @@ defmodule Servy.Fetcher do
   end
 
   def get_result(pid) do
-    receive do { ^pid, :result, value } -> value end
+    receive do
+      { ^pid, :result, value } -> value
+    after :timer.seconds(2) ->
+      raise "Timed out!"
+    end
   end
 
 end
