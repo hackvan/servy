acd20634633cd6df03269e09de46a50d0dd3dde1
Diego Camacho <hackvan@gmail.com>
2021-04-05 17:25:34 -0500

    Make a GenServer to spawn and link the HTTP Server

---
+++ lib/servy/kick_starter.ex
@@ -0,0 +1,34 @@
+defmodule Servy.KickStarter do
+  use GenServer
+
+  # server_pid = Process.whereis(:http_server)
+  # Process.exit(server_pid, :kaboom)
+  # Process.alive?(server_pid)
+  # Process.info(server_pid, :links)
+
+  def start do
+    IO.puts "Starting the kickstarter..."
+    GenServer.start(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  def init(:ok) do
+    Process.flag(:trap_exit, true)
+    server_pid = start_server()
+    {:ok, server_pid}
+  end
+
+  def handle_info({:EXIT, _pid, reason}, _state) do
+    IO.puts "HttpServer exited (#{inspect reason})"
+    server_pid = start_server()
+    {:noreply, server_pid}
+  end
+
+  defp start_server do
+    IO.puts "Starting the HTTP server..."
+    # server_pid = spawn(Servy.HttpServer, :start, [4000])
+    # Process.link(server_pid)
+    server_pid = spawn_link(Servy.HttpServer, :start, [4000])
+    Process.register(server_pid, :http_server)
+    server_pid
+  end
+end
