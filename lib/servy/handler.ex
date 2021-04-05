73cca0d794c32ec918cc987e286ec958ed90d20d
Diego Camacho <hackvan@gmail.com>
2021-04-05 15:35:31 -0500

    Make another GenServer for sensor data and use a cache

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -31,16 +31,9 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{ method: "GET", path: "/sensors"} = conv) do
-    task = Task.async(Servy.Tracker, :get_location, ["bigfoot"])
+    sensor_data = Servy.SensorServer.get_sensor_data()
 
-    snapshots =
-      ["cam-1", "cam-2", "cam-3"]
-      |> Enum.map(&Task.async(Servy.VideoCam, :get_snapshot, [&1]))
-      |> Enum.map(&Task.await/1)
-
-    where_is_bigfoot = Task.await(task)
-
-    render(conv, "sensors.eex", snapshots: snapshots, location: where_is_bigfoot)
+    render(conv, "sensors.eex", snapshots: sensor_data[:snapshots], location: sensor_data[:location])
   end
 
   def route(%Conv{ method: "GET", path: "/kaboom"} = _conv) do
